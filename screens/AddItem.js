028f92ed1c248bfad59d2dfd10ba65972677027c
MuhammadRizwan051 <memonrizwan051@gmail.com>
2022-12-25 19:35:49 +0500

    Changings

--- screens/AddItem.js
+++ screens/AddItem.js
@@ -1,13 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import SMTextInput from '../component/SMTextInput'
+import SMTouchableOpacity from '../component/SMTouchableOpacity'
 
 const AddItem = () => {
   return (
     <View>
       <Text>AddItem</Text>
-      <SMTextInput placeholder='Name' />
-      <SMTextInput placeholder='Price' />
+      <View style={{ alignItems: 'center' }}>
+        <View style={{ width: '80%' }}>
+          <SMTextInput placeholder='Name' style={{ borderWidth: 1 }} />
+          <SMTextInput placeholder='Price' style={{ borderWidth: 1 }} />
+        </View>
+          <SMTouchableOpacity value='Add' touchableStyle={{borderWidth:1, width:'20%'}} textStyle={{textAlign:'center'}} />
+      </View>
     </View>
   )
 }
